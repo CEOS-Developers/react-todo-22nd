54e33a4c51904208fe9247e1b7b14fcf87e18584
hammoooo <chlangjs@g.hongik.ac.kr>
2025-09-20 20:11:42 +0900

    refactor: vercel배포 수정

--- todo-react/src/components/Navbar.tsx
+++ todo-react/src/components/Navbar.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import styled from "styled-components";
 import { addDays, KDate } from "../utils";
 
--- todo-react/src/components/TodoList.tsx
+++ todo-react/src/components/TodoList.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import type { Todo } from "../types";
 import styled, { css } from "styled-components";
 
